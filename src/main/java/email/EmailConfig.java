e9a2529f4aca2e72db3937d715f248bec9d4f225
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-03-11 19:10:16 +0530

    changes-15482

--- src/main/java/email/EmailConfig.java
+++ src/main/java/email/EmailConfig.java
@@ -7,8 +7,7 @@ public class EmailConfig {
 
     public static final String FROM = "*****@gmail.com";
     public static final String PASSWORD = "******8";
-
-    /* "**********@gmail.com", */
+    
     public static final String[] TO = { "****@gmail.com" };
     public static final String SUBJECT = "Test Automation Report";
 }
