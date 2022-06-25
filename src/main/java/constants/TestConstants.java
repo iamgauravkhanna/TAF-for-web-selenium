347baee883764dce3ce580c71a1e3839d9994116
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-06-25 21:23:24 +0530

    changes-2022-06-25-21-23-24

--- src/main/java/constants/TestConstants.java
+++ src/main/java/constants/TestConstants.java
@@ -4,8 +4,6 @@ import java.io.File;
 
 public class TestConstants {
 
-    public static final String FILE_SEPARATOR = System.getProperty("file.separator") ;
-
     public static final int EXPLICIT_WAIT = 10;
     public static final int FLUENT_WAIT = 30;
     public static final int FLUENT_POLLING = 5;
@@ -17,13 +15,16 @@ public class TestConstants {
     public static final String BROWSER_OPERA = "opera";
 
     public static final String USER_DIR = System.getProperty("user.dir");
+
     public static final String DIRECTORY_ENV_CONFIG = "/src/main/resources/";
 
-    public static final String ATTRIBUTE_VALUE = "value";
+    public static final String LOGS_DIR = System.getProperty("logsDirectory");
 
-    public static final String LOG4J_PATH = USER_DIR + File.separator + "log4j.properties";
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
-    public static final String SCREENSHOTS_PATH = System.getProperty("logsDirectory") + File.separator + "screenshots";
+    public static final String LOG4J_PATH = USER_DIR + FILE_SEPARATOR + "log4j.properties";
+
+    public static final String SCREENSHOTS_PATH = LOGS_DIR + FILE_SEPARATOR + "screenshots";
 
     public static final String BASE_URL = "base.url";
 }
